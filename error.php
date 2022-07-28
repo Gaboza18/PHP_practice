49d43788779cf0859311fa8896b9dca6d0190d9e
Gaboza18 <ssychopagi@naver.com>
2022-07-28 13:27:46 +0900

    php test

---
+++ error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>문서의 제목</h1>
+        <?php
+          $name = "홍길동";
+          echo '반갑습니다.' .$name;
+        ?>
+</body>
+</html>
