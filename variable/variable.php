5fc1ece253d6aa041435290936eaede78c03ef74
Gaboza18 <ssychopagi@naver.com>
2022-08-03 13:05:12 +0900

    basic php

---
+++ variable/variable.php
@@ -0,0 +1,17 @@
+<?php
+    $a =1;
+    echo $a+1;
+    echo "<br />";
+
+    $a =2;
+    print $a+1;
+    echo "<br />";
+
+    $first = "coding";
+    echo $first."exe";
+    echo "<br />";
+
+    // 상수
+    define('TITLE','php tutorial');
+    echo TITLE;
+?>
